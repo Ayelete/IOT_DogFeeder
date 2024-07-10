ed1f804adddea8eacb01a8c1ee4e6474dfbc8d2f
Arielbrock <ariel.brock1997@gmail.com>
2024-07-10 22:32:25 +0300

    added a file that gets the time using NTP

---
+++ Unit Tests/timeNTP/timeNTP.ino
@@ -0,0 +1,81 @@
+#include <WiFi.h>
+#include "time.h"
+
+const char * ssid="Ariel";
+const char * wifipw="PandasAreLife";
+
+void setTimezone(String timezone){
+  Serial.printf("  Setting Timezone to %s\n",timezone.c_str());
+  setenv("TZ",timezone.c_str(),1);  //  Now adjust the TZ.  Clock settings are adjusted to show the new local time
+  tzset();
+}
+
+void initTime(String timezone){
+  struct tm timeinfo;
+
+  Serial.println("Setting up time");
+  configTime(0, 0, "pool.ntp.org");    // First connect to NTP server, with 0 TZ offset
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("  Failed to obtain time");
+    return;
+  }
+  Serial.println("  Got the time from NTP");
+  // Now we can set the real timezone
+  setTimezone(timezone);
+}
+
+void printLocalTime(){
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time 1");
+    return;
+  }
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S zone %Z %z ");
+}
+
+void  startWifi(){
+  WiFi.begin(ssid, wifipw);
+  Serial.println("Connecting Wifi");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.print("Wifi RSSI=");
+  Serial.println(WiFi.RSSI());
+}
+
+void setTime(int yr, int month, int mday, int hr, int minute, int sec, int isDst){
+  struct tm tm;
+
+  tm.tm_year = yr - 1900;   // Set date
+  tm.tm_mon = month-1;
+  tm.tm_mday = mday;
+  tm.tm_hour = hr;      // Set time
+  tm.tm_min = minute;
+  tm.tm_sec = sec;
+  tm.tm_isdst = isDst;  // 1 or 0
+  time_t t = mktime(&tm);
+  Serial.printf("Setting time: %s", asctime(&tm));
+  struct timeval now = { .tv_sec = t };
+  settimeofday(&now, NULL);
+}
+
+void setup(){
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  startWifi();
+
+  initTime("IST-2IDT,M3.4.4/26,M10.5.0");   // Israel 
+  printLocalTime();
+}
+
+void loop() {
+  int i;
+  
+  // put your main code here, to run repeatedly:
+  for(i=0; i<10; i++){
+    delay(1000);
+    printLocalTime();
+  }
+
+}
