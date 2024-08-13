750cb853765d1523291badd20537d51084230d1f
Ayelete <ayeleteb@campus.technion.ac.il>
2024-08-13 11:48:13 +0300

    added statistics screen + all notifications (still one little bug in one king of notification)

--- FlutterSupportApp/lib/main.dart
+++ FlutterSupportApp/lib/main.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'home_page.dart';
+import 'notification_service.dart';
+import 'firestore_service.dart'; // Ajoutez cette ligne
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 void main() async {
+  // Initialize the widgets
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize the Firebase database
   await Firebase.initializeApp();
+
+  // Initialize the local notifications
+  final NotificationService notificationService = NotificationService();
+  await notificationService.initialize();
+
+  // Initialize Firestore service
+  final FirestoreService firestoreService = FirestoreService(notificationService);
+  firestoreService.startListening(); // Commence à écouter les changements dans Firestore
+
   runApp(MyApp());
 }
 
@@ -16,7 +35,25 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      navigatorKey: navigatorKey,
       home: HomePage(),
+      routes: {
+        '/specificScreen': (context) => SpecificScreen(),
+      },
+    );
+  }
+}
+
+class SpecificScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Specific Screen'),
+      ),
+      body: Center(
+        child: Text('This is the specific screen'),
+      ),
     );
   }
 }
